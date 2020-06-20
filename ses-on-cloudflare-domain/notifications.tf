1b22a9efae058b7c134f52bde59fd57b97c24443
Rami Al-Ghanmi <rami@ramialghanmi.com>
2020-06-20 01:23:20 -0700

    Create SNS topics to receive complaints and bounced messages

---
+++ ses-on-cloudflare-domain/notifications.tf
@@ -0,0 +1,23 @@
+resource "aws_sns_topic" "bounce" {
+  name = format("%s-ses-bounce", replace(aws_ses_domain_identity.default.domain, ".", "-"))
+  tags = var.tags
+}
+
+resource "aws_ses_identity_notification_topic" "bounce" {
+  topic_arn                = aws_sns_topic.bounce.arn
+  notification_type        = "Bounce"
+  identity                 = aws_ses_domain_identity.default.domain
+  include_original_headers = true
+}
+
+resource "aws_sns_topic" "complaint" {
+  name = format("%s-ses-complaint", replace(aws_ses_domain_identity.default.domain, ".", "-"))
+  tags = var.tags
+}
+
+resource "aws_ses_identity_notification_topic" "complaint" {
+  topic_arn                = aws_sns_topic.complaint.arn
+  notification_type        = "Complaint"
+  identity                 = aws_ses_domain_identity.default.domain
+  include_original_headers = true
+}
