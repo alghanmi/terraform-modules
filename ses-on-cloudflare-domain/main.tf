4ed427451a4dc5573575c14c5fe87e674cfa5e76
Rami Al-Ghanmi <rami@ramialghanmi.com>
2020-06-19 22:24:50 -0700

    DKIM, SPF and MX records setup

--- ses-on-cloudflare-domain/main.tf
+++ ses-on-cloudflare-domain/main.tf
@@ -1,23 +1,25 @@
+## SES Domain Verification
 resource "aws_ses_domain_identity" "default" {
   domain = var.domain
 }
 
 resource "cloudflare_record" "ses_verification" {
   zone_id = var.domain_zone_id
-  name    = "_amazonses.${aws_ses_domain_identity.default.id}"
+  name    = "_amazonses.${aws_ses_domain_identity.default.domain}"
   type    = "TXT"
   ttl     = "3600"
   value   = aws_ses_domain_identity.default.verification_token
 }
 
 resource "aws_ses_domain_identity_verification" "default" {
-  domain = aws_ses_domain_identity.default.id
+  domain = aws_ses_domain_identity.default.domain
 
   depends_on = [cloudflare_record.ses_verification]
 }
 
+## DKIM Setup
 resource "aws_ses_domain_dkim" "default" {
-  domain     = aws_ses_domain_identity.default.domain
+  domain = aws_ses_domain_identity.default.domain
 
   depends_on = [aws_ses_domain_identity_verification.default]
 }
@@ -27,6 +29,25 @@ resource "cloudflare_record" "default_dkim" {
   zone_id = var.domain_zone_id
   name    = "${aws_ses_domain_dkim.default.dkim_tokens[count.index]}._domainkey.${aws_ses_domain_dkim.default.domain}"
   type    = "CNAME"
-  ttl     = "600"
+  ttl     = "3600"
   value   = "${aws_ses_domain_dkim.default.dkim_tokens[count.index]}.dkim.amazonses.com"
+}
+
+## SPF Records
+resource "cloudflare_record" "default_spf" {
+  zone_id = var.domain_zone_id
+  name    = aws_ses_domain_identity.default.domain
+  type    = "TXT"
+  value   = var.spf_record
+  ttl     = "600"
+}
+
+## MX Record
+resource "cloudflare_record" "default_mx" {
+  zone_id  = var.domain_zone_id
+  name     = aws_ses_domain_identity.default.domain
+  type     = "MX"
+  value    = "inbound-smtp.${var.ses_region}.amazonaws.com"
+  ttl      = "600"
+  priority = 10
 }
--- ses-on-cloudflare-domain/variables.tf
+++ ses-on-cloudflare-domain/variables.tf
@@ -7,3 +7,14 @@ variable "domain_zone_id" {
   type        = string
   description = "Cloudflare zone id"
 }
+
+variable "ses_region" {
+  type        = string
+  description = "AWS region for the SES sending endpoint"
+}
+
+variable "spf_record" {
+  type        = string
+  description = "Sender Policy Framework (SPF) for SES. https://docs.aws.amazon.com/ses/latest/DeveloperGuide/send-email-authentication-spf.html"
+  default     = "v=spf1 include:amazonses.com -all"
+}
