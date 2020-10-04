8de1093f6181ac836e6c3909d591efb73393e70c
Rami Al-Ghanmi <rami@ramialghanmi.com>
2020-10-04 16:41:02 -0700

    Switch to terraform 0.13

---
+++ static-s3-site-on-cloudflare/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.9.0"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.11.0"
+    }
+  }
+
+  required_version = ">= 0.13"
+}
