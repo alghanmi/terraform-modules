8806d0988fd358769bc69255136014bfe3f8dc31
Rami Al-Ghanmi <rami@ramialghanmi.com>
2020-12-05 12:15:19 -0800

    Upgrade provider versions

--- ses-on-cloudflare-domain/versions.tf
+++ ses-on-cloudflare-domain/versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.9.0"
+      version = "~> 3.20.0"
     }
 
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 2.11.0"
+      version = "~> 2.14.0"
     }
   }
 
